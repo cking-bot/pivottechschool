ab613448306a3f7e2294be4bcb2fc402d194f330
itscharlzard <kingcharlie1103@yahoo.com>
2022-11-26 10:39:50 -0800

    created marvel client and marvel main.go file

---
+++ marvel/types.go
@@ -0,0 +1,23 @@
+package marvel
+
+type Characters struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type CharactersResponse struct {
+	Code            int    `json:"code"`
+	Status          string `json:"status"`
+	Copyright       string `json:"copyright"`
+	AttributionText string `json:"attributionText"`
+	AttributionHTML string `json:"attributionHTML"`
+	Etag            string `json:"etag"`
+	Data            struct {
+		Offset  int          `json:"offset"`
+		Limit   int          `json:"limit"`
+		Total   int          `json:"total"`
+		Count   int          `json:"count"`
+		Results []Characters `json:"results"`
+	} `json:"data"`
+}
