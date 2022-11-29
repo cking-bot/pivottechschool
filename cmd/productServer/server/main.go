52a298eea3b22693c73f9d8c7886ee2ed857874d
itscharlzard <kingcharlie1103@yahoo.com>
2022-11-29 12:22:26 -0800

    Completed Part 2
    Refactored product Server main.go file

--- cmd/productServer/server/main.go
+++ cmd/productServer/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"log"
 	"net/http"
 	"os"
@@ -10,241 +12,221 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Connection struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
 type Product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
 }
 
 var products []Product
 
-func initProducts() {
-
-	bs, err := os.ReadFile("products.json")
+func initConnection(filepath string) (*gorm.DB, error) {
+	db, err := gorm.Open("sqlite3", filepath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	if err := json.Unmarshal(bs, &products); err != nil {
-		log.Fatal(err)
+	err = db.DB().Ping()
+	if err != nil {
+		return nil, err
 	}
+
+	log.Println("Created new Database")
+	return db, nil
 }
 
-func getProductsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(products); err != nil {
+func (c *Connection) getProductsHandler(w http.ResponseWriter, r *http.Request) {
+	limiter := r.URL.Query().Get("limit")
+	if limiter == "" {
+		limiter = "10"
+	}
+	sortBy := r.URL.Query().Get("sort")
+	if sortBy == "" {
+		sortBy = "id"
+	}
+
+	limitInteger, _ := strconv.Atoi(limiter)
+	if limitInteger > 100 {
+		limitInteger = 100
+	}
+	if limitInteger < 0 {
+		limitInteger = 10
+	}
+	switch sortBy {
+	case "id":
+	case "name":
+	case "description":
+	case "price":
+	default:
+		sortBy = "id"
+
+	}
+
+	var prodRet []Product
+	c.DB.Table("products").Select("*").Limit(limitInteger).Order(sortBy).Scan(&prodRet)
+	log.Println(prodRet)
+
+	rep, err := json.Marshal(&prodRet)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(rep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
 
-func getProductsIdHandler(w http.ResponseWriter, r *http.Request) {
+func (c *Connection) getProductsIdHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	ID, err := strconv.Atoi(vars["id"])
+	ID := vars["id"]
+
+	if _, err := strconv.Atoi(ID); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var prodRet Product
+	c.DB.Table("products").First(&prodRet, "ID = ?", ID).Scan(&prodRet)
+
+	if prodRet.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	rep, err := json.Marshal(&prodRet)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	for _, product := range products {
-		if product.ID == ID {
-			err := json.NewEncoder(w).Encode(product)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			} //handles the encoding error and returns the correct status
-			return
-
-		} //if the product is equal to the ID encode and send the information,
-
+	_, err = w.Write(rep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-
-	w.WriteHeader(http.StatusNotFound)
-	return //this means it is not in the slice at all
 }
 
-func createProductsHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("objects/products.json")
+func (c *Connection) createProductsHandler(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	err := json.NewDecoder(r.Body).Decode(&product)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	var products []Product
-	err = json.Unmarshal(data, &products)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	id, err := strconv.Atoi(r.FormValue("id"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	price, _ := strconv.Atoi(r.FormValue("price"))
-	if r.FormValue("price") == "" || r.FormValue("description") == "" || r.FormValue("name") == "" || r.FormValue("id") == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	for _, product := range products {
-		if product.ID == id {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-	}
-
-	newProduct := Product{
-		ID:          id,
-		Price:       price,
-		Name:        r.FormValue("name"),
-		Description: r.FormValue("description"),
-	}
-
-	products = append(products, newProduct)
-
-	productsByte, err := json.Marshal(products)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	file, err := os.Create("objects/products.json")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	defer file.Close()
-
-	_, writeErr := file.Write(productsByte)
-	if writeErr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-func updateProductsHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("objects/products.json")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	var products []Product
-	err = json.Unmarshal(data, &products)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	price, err := strconv.Atoi(r.FormValue("price"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	if r.FormValue("price") == "" || r.FormValue("description") == "" || r.FormValue("name") == "" {
+	if product.ID == 0 || product.Name == "" || product.Price == 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	newProduct := Product{
-		ID:          id,
-		Price:       price,
-		Name:        r.FormValue("name"),
-		Description: r.FormValue("description"),
+	var prodRet Product
+	c.DB.Table("products").First(&prodRet, "ID = ?", product.ID).Scan(&prodRet)
+	if prodRet.ID == product.ID {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-
-	for i, product := range products {
-		if product.ID == id {
-			products[i] = newProduct
-
-			productsByte, _ := json.Marshal(products)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			file, err := os.Create("objects/products.json")
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			defer file.Close()
-
-			_, writeErr := file.Write(productsByte)
-			if writeErr != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			return
-		}
-	}
-	w.WriteHeader(http.StatusBadRequest)
-	return
+	c.DB.Create(&product)
+	w.WriteHeader(http.StatusCreated)
 }
 
-func deleteProductHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("objects/products.json")
+func (c *Connection) updateProductsByIdHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ID := vars["id"]
+
+	if _, err := strconv.Atoi(ID); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var product Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if product.ID == 0 || product.Name == "" || product.Price == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var prodRet Product
+	c.DB.Table("products").First(&prodRet, "ID = ?", ID).Scan(&prodRet)
+	if prodRet.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	idInteger, err := strconv.Atoi(ID)
+	if idInteger == product.ID {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	c.DB.Model(&products).Where("id = ?").Updates(Product{ID: product.ID, Name: product.Name, Description: product.Description, Price: product.Price})
+	w.WriteHeader(http.StatusOK)
+}
+
+func (c *Connection) deleteProductHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ID := vars["id"]
+
+	if _, err := strconv.Atoi(ID); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	idInteger, err := strconv.Atoi(ID)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	if idInteger == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
-	var products []Product
-	err = json.Unmarshal(data, &products)
-
-	for i, product := range products {
-		if product.ID == id {
-			products = append(products[:i], products[i+1:]...)
-
-			productsByte, _ := json.Marshal(products)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			file, err := os.Create("objects/products.json")
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			defer file.Close()
-
-			_, writeErr := file.Write(productsByte)
-			if writeErr != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			return
-		}
+	var check Product //looks for product
+	c.DB.Table("products").First(&check, "ID = ?", ID).Scan(&check)
+	if check.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
-	w.WriteHeader(http.StatusBadRequest)
-	return
+
+	c.DB.Where("ID = ?", ID).Delete(&Product{}) //deletes
+
+	var prodRet Product //checks to make sure it was deleted
+	c.DB.Table("products").First(&prodRet, "ID = ?", ID).Scan(prodRet)
+	if prodRet.ID == 0 {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	w.WriteHeader(http.StatusInternalServerError)
 }
 
 func main() {
-	initProducts()
-	r := mux.NewRouter()
-	r.HandleFunc("/products", getProductsHandler).Methods("GET")
-	r.HandleFunc("/products/{id}", getProductsIdHandler).Methods("GET")
-	r.HandleFunc("/products", createProductsHandler)
-	r.HandleFunc("/products/{id}", updateProductsHandler).Methods("PUT")
-	r.HandleFunc("products/{id}", deleteProductHandler).Methods("DELETE")
+	var err error
+	c := Connection{}
+	c.Router = mux.NewRouter()
+
+	c.DB, err = initConnection("../../data-driven-product-server/seeder/products.db")
+	if err != nil {
+		log.Printf("Can't connect to Database\n%v", err)
+		os.Exit(1)
+	}
+
+	c.Router.HandleFunc("/products", c.getProductsHandler).Methods("GET")
+	c.Router.HandleFunc("/products", c.createProductsHandler).Methods("POST")
+	c.Router.HandleFunc("/products/{id}", c.getProductsIdHandler).Methods("GET")
+	c.Router.HandleFunc("/products/{id}", c.updateProductsByIdHandler).Methods("PUT")
+	c.Router.HandleFunc("products/{id}", c.deleteProductHandler).Methods("DELETE")
 	// Bind to a port and pass our router in
 	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	log.Fatal(http.ListenAndServe("localhost:8080", c.Router))
 
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,12 @@ require github.com/gorilla/mux v1.8.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.4.0
+	github.com/mattn/go-sqlite3 v1.14.16
 )
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
