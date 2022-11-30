171f824940dd670e005008866a8867760e879407
itscharlzard <kingcharlie1103@yahoo.com>
2022-11-29 18:15:36 -0800

    updated client.go/types.go

--- marvel/client.go
+++ marvel/client.go
@@ -11,36 +11,22 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/joho/godotenv"
 )
 
-func NewClient() {
-	err := godotenv.Load()
-	if err != nil {
+var pubkey, privkey = keys()
+var BaseURL = "https://gateway.marvel.com/v1/public"
+var httpClient = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+func keys() (public, private string) {
+	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	pubkey := os.Getenv("MARVEL_PUBLIC_KEY")
 	privkey := os.Getenv("MARVEL_PRIVATE_KEY")
-
-	client := marvelClient{
-		baseURL: "https://gateway.marvel.com/v1/public",
-		pubkey:  pubkey,
-		privkey: privkey,
-		httpClient: &http.Client{
-			Timeout: 10 * time.Second,
-		},
-	}
-
-	characters, err := client.getCharacters()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, character := range characters {
-		fmt.Println(character.Name, character.Description)
-	}
+	return pubkey, privkey
 }
 
 type marvelClient struct {
@@ -48,6 +34,16 @@ type marvelClient struct {
 	pubkey     string
 	privkey    string
 	httpClient *http.Client
+}
+
+func NewClient(url string) marvelClient {
+
+	return marvelClient{
+		url,
+		pubkey,
+		privkey,
+		httpClient,
+	}
 }
 
 func (c *marvelClient) md5Hash(ts int64) string {
@@ -59,13 +55,12 @@ func (c *marvelClient) md5Hash(ts int64) string {
 func (c *marvelClient) signURL(url string) string {
 	ts := time.Now().Unix()
 	hash := c.md5Hash(ts)
-	return fmt.Sprintf("%s&ts=%d&apikey=%s&has=%s", url, ts, c.pubkey, hash)
+	return fmt.Sprintf("%s&ts=%d&apikey=%s&hash=%s", url, ts, c.pubkey, hash)
 }
 
-func (c *marvelClient) getCharacters() ([]Characters, error) {
-	url := c.baseURL + "/events?limit=25"
-	url = c.signURL(url)
-	spew.Dump(url)
+func (c *marvelClient) GetCharacters() ([]Character, error) {
+	url := c.baseURL + "/characters?limit=25"
+	url = c.signURL((url))
 
 	res, err := c.httpClient.Get(url)
 	if err != nil {
@@ -73,11 +68,9 @@ func (c *marvelClient) getCharacters() ([]Characters, error) {
 	}
 	defer res.Body.Close()
 
-	spew.Dump(res.Status, res.StatusCode)
-
-	var characterResponse CharactersResponse
-	if err := json.NewDecoder(res.Body).Decode(&characterResponse); err != nil {
+	var characterRes CharactersResponse
+	if err := json.NewDecoder(res.Body).Decode(&characterRes); err != nil {
 		return nil, err
 	}
-	return characterResponse.Data.Results, nil
+	return characterRes.Data.Results, nil
 }
